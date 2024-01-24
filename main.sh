d3ee8efd740ef579ff7dfa7f1f5e633d42bdfd16
Jorge Morante <jorge@morante.eu>
2024-01-24 15:46:54 +0100

    download agent and run on port 4444

--- main.sh
+++ main.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-echo "hello kitty"
+export KITTENGRID_VCS_PROVIDER="github"
+export KITTENGRID_VCS_ID="$1"
+export KITTENGRID_BIND_ADDRESS="0.0.0.0"
+export KITTENGRID_WORKFLOW_ID=$GITHUB_RUN_ID
+export KITTENGRID_API_URL="https://kittengrid.com"
+
+mkdir -p /tmp/kittengrid/bin
+wget https://releases.kittengrid.com/kittengrid-agent/0.0.1/kittengrid-agent_0.0.1_linux_amd64.zip -O /tmp/kittengrid-agent.zip
+unzip /tmp/kittengrid-agent.zip -d /tmp/kittengrid/bin
+
+sudo -E /tmp/kittengrid/bin/kittengrid-agent --bind-port 4444
