b72697cf34c63a38cfe656772a378bb285239fe8
Jorge Morante <jorge@morante.eu>
2024-01-30 15:00:28 +0100

    wip

--- main.sh
+++ main.sh
@@ -10,6 +10,10 @@ response=$( \
         }"
 )
 
-echo "---response---"
-echo $response
-echo "--------------"
+# Poor mans jq
+AGENT_TOKEN=$(echo $response | cut -f4 -d '"')
+
+curl -XPUT http://localhost:3000/api/agents \
+       --header 'Content-Type: application/json' \
+       --header "Authorization: Bearer $AGENT_TOKEN" \
+       --data "{ \"workflow_id\": \"$GITHUB_RUN_ID\" }"
