5251f695d558455316feb8d32fe42397b45d7b35
Jorge Morante <jorge@morante.eu>
2024-05-10 13:08:07 +0200

    wip

--- main.sh
+++ main.sh
@@ -12,4 +12,4 @@ mkdir -p /tmp/kittengrid/bin
 wget https://releases.kittengrid.com/kittengrid-agent/0.0.1/kittengrid-agent_0.0.1_linux_amd64.zip -O /tmp/kittengrid-agent.zip
 unzip /tmp/kittengrid-agent.zip -d /tmp/kittengrid/bin
 
-sudo -E /tmp/kittengrid/bin/kittengrid-agent
+sudo -E /tmp/kittengrid/bin/kittengrid-agent --log-level debug
