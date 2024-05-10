b7af84f5284c75ca02e1010d386abb9543059892
Jorge Morante <jorge@morante.eu>
2024-05-10 11:40:12 +0200

    wip

--- main.sh
+++ main.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env bash
 
-response=$( \
-  curl -XPOST http://localhost:3000/api/agents/register \
-       --header 'Content-Type: application/json' \
-       --header "Authorization: Bearer $KITTENGRID_API_KEY" \
-       --data "{
-          \"vcs_provider\": \"github\",
-          \"vcs_id\": \"$1\"
-        }"
-)
+export KG_API_KEY=$KITTENGRID_API_KEY
+export KG_API_URL="https://kittengrid.com/"
+export KG_VCS_PROVIDER="github"
+export KG_VCS_ID="$1"
+export KG_WORKFLOW_ID=$GITHUB_RUN_ID
 
-# Poor mans jq
-AGENT_TOKEN=$(echo $response | cut -f4 -d '"')
+mkdir -p /tmp/kittengrid/bin
+wget https://releases.kittengrid.com/kittengrid-agent/0.0.1/kittengrid-agent_0.0.1_linux_amd64.zip -O /tmp/kittengrid-agent.zip
+unzip /tmp/kittengrid-agent.zip -d /tmp/kittengrid/bin
 
-curl -XPUT http://localhost:3000/api/agents \
-       --header 'Content-Type: application/json' \
-       --header "Authorization: Bearer $AGENT_TOKEN" \
-       --data "{ \"workflow_id\": \"$GITHUB_RUN_ID\" }"
+/tmp/kittengrid/bin/kittengrid-agent
