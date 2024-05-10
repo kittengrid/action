95f8c32c9292ebb4bf568a74605c5b0ad572bbd9
Jorge Morante <jorge@morante.eu>
2024-05-10 12:34:55 +0200

    wip

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ export KG_API_URL="https://kittengrid.com/"
 export KG_VCS_PROVIDER="github"
 export KG_VCS_ID="$1"
 export KG_WORKFLOW_ID=$GITHUB_RUN_ID
-export KG_LOG_LEVEL=trace
+export LOG_LEVEL=debug
 
 mkdir -p /tmp/kittengrid/bin
 wget https://releases.kittengrid.com/kittengrid-agent/0.0.1/kittengrid-agent_0.0.1_linux_amd64.zip -O /tmp/kittengrid-agent.zip
