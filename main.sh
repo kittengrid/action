0c402c060d4c55755169c26616199eb091ccf8b9
Jose Fernandez (magec) <joseferper@gmail.com>
2024-05-23 13:31:26 +0200

    s/KG_/KITTENGRID_/g

--- main.sh
+++ main.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-export KG_API_KEY=$KITTENGRID_API_KEY
-export KG_API_URL="https://kittengrid.com"
-export KG_VCS_PROVIDER="github"
-export KG_VCS_ID="$1"
-export KG_BIND_ADDRESS="127.0.0.1"
-export KG_WORKFLOW_ID=$GITHUB_RUN_ID
+export KITTENGRID_API_KEY=$KITTENGRID_API_KEY
+export KITTENGRID_API_URL="https://kittengrid.com"
+export KITTENGRID_VCS_PROVIDER="github"
+export KITTENGRID_VCS_ID="$1"
+export KITTENGRID_BIND_ADDRESS="127.0.0.1"
+export KITTENGRID_WORKFLOW_ID=$GITHUB_RUN_ID
 
 mkdir -p /tmp/kittengrid/bin
 wget https://releases.kittengrid.com/kittengrid-agent/0.0.1/kittengrid-agent_0.0.1_linux_amd64.zip -O /tmp/kittengrid-agent.zip
