e1ca5598410a7470f07e31a17dd428a1293ebc15
Jorge Morante <jorge@morante.eu>
2024-09-19 11:33:07 +0200

    add project and pull request vcs id

--- main.sh
+++ main.sh
@@ -1,7 +1,30 @@
 #!/usr/bin/env bash
 
+for i in "$@"; do
+  case $i in
+    --repo)
+      PROJECT_VCS_ID=$2
+      shift 2
+      ;;
+    --pull-request)
+      PULL_REQUEST_VCS_ID=$2
+      shift 2
+  esac
+done
+
+if [ -z $PULL_REQUEST_VCS_ID ]; then
+  echo "Missing argument: --pull-request-vcs-id"
+  exit 1;
+fi
+
+if [ -z $PROJECT_VCS_ID ]; then
+  echo "Missing argument: --project-vcs-id"
+  exit 1;
+fi
+
 export KITTENGRID_VCS_PROVIDER="github"
-export KITTENGRID_VCS_ID="$1"
+export KITTENGRID_PROJECT_VCS_ID="$PROJECT_VCS_ID"
+export KITTENGRID_PULL_REQUEST_VCS_ID="$PULL_REQUEST_VCS_ID"
 export KITTENGRID_BIND_ADDRESS="0.0.0.0"
 export KITTENGRID_WORKFLOW_ID=$GITHUB_RUN_ID
 export KITTENGRID_API_URL="https://kittengrid.com"
