35810c3f34eca87fdde898c77017629ab45c6a10
Jorge Morante <jorge@morante.eu>
2024-09-19 11:34:55 +0200

    allow KITTENGRID_API_URL to be set externally

--- main.sh
+++ main.sh
@@ -27,7 +27,10 @@ export KITTENGRID_PROJECT_VCS_ID="$PROJECT_VCS_ID"
 export KITTENGRID_PULL_REQUEST_VCS_ID="$PULL_REQUEST_VCS_ID"
 export KITTENGRID_BIND_ADDRESS="0.0.0.0"
 export KITTENGRID_WORKFLOW_ID=$GITHUB_RUN_ID
-export KITTENGRID_API_URL="https://kittengrid.com"
+
+if [ -z $KITTENGRID_API_URL ]; then
+  export KITTENGRID_API_URL="https://kittengrid.com"
+fi
 
 mkdir -p /tmp/kittengrid/bin
 wget https://releases.kittengrid.com/kittengrid-agent/0.0.1/kittengrid-agent_0.0.1_linux_amd64.zip -O /tmp/kittengrid-agent.zip
