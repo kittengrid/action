f7c8ff3c608a4fd3f9c3898ee07abae603c2fe22
Jorge Morante <jorge@morante.eu>
2024-11-15 11:50:20 +0100

    fix KITTENGRID_API_URL

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@
 export KITTENGRID_VCS_PROVIDER="github"
 export KITTENGRID_VCS_ID="$1"
 export KITTENGRID_WORKFLOW_RUN_ID=$GITHUB_RUN_ID
-export KITTENGRID_API_URL="https://kittengrid.com"
+export KITTENGRID_API_URL="https://app.kittengrid.com"
 
 mkdir -p /tmp/kittengrid/bin
 wget https://releases.kittengrid.com/kittengrid-agent/0.0.1/kittengrid-agent_0.0.1_linux_amd64.zip -O /tmp/kittengrid-agent.zip
